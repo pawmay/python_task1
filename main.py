c204f175818e5612c961934a63fef0e08e18cade
Paweł Maywald <pawmay@gmail.com>
2021-08-16 17:24:46 +0200

    add next months

--- main.py
+++ main.py
@@ -15,3 +15,157 @@ loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
 loan_amt_diff = loan_amt - loan_amt_new
 print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
 loan_amt = loan_amt_new
+
+# March
+inflation = 2.32467171712441
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# April
+inflation = 1.26125440724877
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# May
+inflation = 1.78252628571251
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# June
+inflation = 2.32938454145522
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# July
+inflation = 1.50222984223283
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# August
+inflation = 1.78252628571251
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# September
+inflation = 2.32884899407637
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# October
+inflation = 0.616921348207244
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# November
+inflation = 2.35229588637833
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# December
+inflation = 0.337779545187098
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# January
+inflation = 1.57703524727525
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# February
+inflation = -0.292781442607648
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# March
+inflation = 2.48619659017508
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# April
+inflation = 0.267110317834564
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# May
+inflation = 1.41795267229799
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# June
+inflation = 1.05424326726375
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# July
+inflation = 1.4805201044812
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# August
+inflation = 1.57703524727525
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# September
+inflation = -0.0774206903147018
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# October
+inflation = 1.16573339872354
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# November
+inflation = -0.404186717638335
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
+
+# December
+inflation = 1.49970852083123
+loan_amt_new = (1 + ((inflation + loan_rate) / 1200)) * loan_amt - loan_emi
+loan_amt_diff = loan_amt - loan_amt_new
+print(f"Twoja pozostała kwota kredytu to {loan_amt_new} PLN, to {loan_amt_diff} PLN mniej niż w poprzednim miesiącu.")
+loan_amt = loan_amt_new
